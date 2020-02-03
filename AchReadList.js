7690905fcc025912dee6d30f711f1509d5b5b990
NoshinTahsin <noshintahsiniitdu@gmail.com>
2020-02-03 12:48:27 +0600

    First Commit

---
+++ AchReadList.js
@@ -0,0 +1,45 @@
+function AchReadList(){
+    
+    function AchReadList() {
+        return [ 
+            {
+                id: 1, img: 'assets/default.jfif',
+                title: 'Requirements Engineering — Elicitation & Analysis (Part 2)',
+                des: `The activity of generating 
+                the requirements of a system from users, customers and other stakeholders."
+                `
+            
+            },
+            {
+                id: 2, img: 'assets/default.jfif',
+                title: 'Review',
+                des: "Answering the WHYs"
+            },
+            {
+                id: 3, img: 'assets/default.jfif',
+                title: 'Review',
+                des:'Know where this course is available'
+            },
+            {
+                id: 4, img: 'assets/default.jfif',
+                title: 'Review',
+                des: 'Get in touch with the professors'
+            },
+            {
+                id: 5, img: 'assets/default.jfif',
+                title: ' Review',
+                des: 'Details'
+            },
+            {
+                id: 6, img: 'assets/default.jfif',
+                title: 'Review',
+                des: 'guidance'
+            }
+        ]
+    }
+
+    return {
+        
+        AchReadList
+    }
+}
